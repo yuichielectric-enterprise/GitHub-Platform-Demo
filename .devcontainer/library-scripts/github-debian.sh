21a40e6d6f350440cc5f8b283a78f7f7888c5114
James Ha <62304682+James-LeHa@users.noreply.github.com>
2021-02-18 18:36:28 +0000

    use older cli version

--- .devcontainer/library-scripts/github-debian.sh
+++ .devcontainer/library-scripts/github-debian.sh
@@ -34,8 +34,16 @@ if [ "${CLI_VERSION}" = "latest" ] ||  [ "${CLI_VERSION}" = "current" ] ||  [ "$
 fi
 
 # Install the GitHub CLI
+# echo "Downloading github CLI..."
+# curl -OsSL https://github.com/cli/cli/releases/download/v${CLI_VERSION}/gh_${CLI_VERSION}_linux_amd64.deb
+# echo "Installing github CLI..."
+# apt-get install ./gh_${CLI_VERSION}_linux_amd64.deb
+# echo "Removing github CLI deb file after installation..."
+# rm -rf ./gh_${CLI_VERSION}_linux_amd64.deb
+# echo "Done!"
+
 echo "Downloading github CLI..."
-curl -OsSL https://github.com/cli/cli/releases/download/v${CLI_VERSION}/gh_${CLI_VERSION}_linux_amd64.deb
+curl -OsSL https://github.com/cli/cli/releases/download/v1.3.1/gh_1.3.1_linux_arm64.deb
 echo "Installing github CLI..."
 apt-get install ./gh_${CLI_VERSION}_linux_amd64.deb
 echo "Removing github CLI deb file after installation..."
